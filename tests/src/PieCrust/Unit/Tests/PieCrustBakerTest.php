2a13669913c890931b8b905363c46302114527e9
Sebastian B. Knapp <news@young-workers.de>
2018-06-11 10:47:14 +0200

    start Unit testing, maybe it is already done?

---
+++ tests/src/PieCrust/Unit/Tests/PieCrustBakerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PieCrust\Unit\Tests;
+
+use PieCrust\Tests\PieCrustTestCase;
+
+use PieCrust\PieCrust;
+use PieCrust\Baker\PieCrustBaker;
+use PieCrust\Mock\MockFileSystem;
+use PieCrust\Mock\MockPage;
+use PieCrust\Util\UriBuilder;
+
+
+class PieCrustBakerTest extends PieCrustTestCase
+{
+    public function testDefaults()
+    {
+        $fs = MockFileSystem::create();
+        $app = $fs->getApp();
+        $baker = new PieCrustBaker($app);
+        $this->assertEquals($baker->getBakeDir(),'');
+    }
+
+    public function testDefaultPageBaker()
+    {
+        $fs = MockFileSystem::create();
+        $app = $fs->getApp();
+        $baker = new PieCrustBaker($app);
+        $page = new MockPage($app);
+
+        $pagebaker = $baker->getPageBaker();
+        $this->assertEquals($pagebaker->getBakedFiles(),array());
+        $this->assertEquals($pagebaker->getOutputPath($page),'/index.html');
+        $this->assertEquals($pagebaker->getPageCount(),0);
+    }
+}
